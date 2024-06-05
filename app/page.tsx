d9b0a36a13998380bc001216ae3931e6f4eaec7a
rashedur-refat <refat@hypersystems.com.bd>
2024-06-05 17:13:05 +0600

    add contact section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Intro from "@/components/intro";
 import About from "@/components/about";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
+import Contact from "@/components/contact";
 
 export default function Home() {
 	return (
@@ -12,6 +13,7 @@ export default function Home() {
 			<About />
 			<Skills />
 			<Experience />
+			<Contact />
 		</main>
 	);
 }
